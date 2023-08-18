46d6dffde7dccf7bd4e1fc0b2f538e295b9f0d69
23lucky2me <128039749+23lucky2me@users.noreply.github.com>
2023-08-18 16:22:12 +0800

    Update client.cpp

--- 2.server_client/client.cpp
+++ 2.server_client/client.cpp
@@ -18,6 +18,24 @@ void sys_err(const char *str){
 }
 
 int main(int argc,char *argv[]){
+  int count = 10;
+  char buf[BUFSIZE];
+  struct sockaddr_in serv_addr;   //服务器的地址结构
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(SERV_PORT);
+  inet_pton(AF_INET,"127.0.0.1",&serv_addr.sin_addr.s_addr);
+  
+  int cfd = socket(AF_INET,SOCK_STREAM,0);
+  if(cfd == -1){
+    sys_err("socket error");
+  }
 
-return 0；
+  connect(cfd,(struct sockaddr *)serv_addr,sizeof(serv_addr));
+  while(--count){
+    write(cfd,"hello",5);
+    int ret = read(cfd,buf,sizeof(buf));
+    write(STDOUT_FILENO,buf,ret);
+  }
+  close(cfd);
+  return 0；
 }
