60052802ebf1dfb4502d6284a596a54e911ef94d
23lucky2me <128039749+23lucky2me@users.noreply.github.com>
2023-08-18 16:10:57 +0800

    Create client.cpp

---
+++ 2.server_client/client.cpp
@@ -0,0 +1,23 @@
+/*  C/S模型下的客户端   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <sys/socket.h>
+
+#define SERV_PORT 9527
+
+void sys_err(const char *str){
+  perror(str)
+  exit(1)
+}
+
+int main(int argc,char *argv[]){
+
+return 0；
+}
